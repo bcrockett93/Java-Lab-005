f26044b32fcfa0a5cc00959eb60316cfee5e706c
Brandin Crockett <brandincrockett@gmail.com>
2023-02-24 14:51:43 -0800

    Completed Lab 005

---
+++ src/Dice.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Brandin Crockett
+ *
+ * @since Version 1.0
+ *
+ */
+import java.util.Random;
+
+public class Dice {
+    private int sideFacingUp;
+    private int sides;
+    private Random randomGenerator;
+
+    public Dice(int sides) {
+        this.sides = sides;
+        this.randomGenerator = new Random();
+    }
+
+    public void roll() {
+        this.sideFacingUp = this.randomGenerator.nextInt(this.sides) + 1;
+    }
+
+    public int view() {
+        return this.sideFacingUp;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Brandin Crockett
+ *
+ * @since Version 1.0
+ *
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Player 1's Name: ");
+        String playerName = s.nextLine();
+        Player p1 = new Player(playerName);
+        System.out.println("Enter Player 2's Name: ");
+        playerName = s.nextLine();
+        Player p2 = new Player(playerName);
+        System.out.println("What sided die are we playing with? ");
+        int sides = Integer.parseInt(s.nextLine());
+
+        Game g = new Game(p1, p2, new Dice(sides));
+        g.play();
+    }
+}
